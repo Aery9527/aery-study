7c4cf4eb13cfb91d37290283295da4ecb0055908
Aery <nulllifeart@gmail.com>
2024-02-12 15:41:58 +0800

    repaired java-native

--- java-native/src/test/java/org/aery/study/java/executors/NewSingleThreadExecutorStudy.java
+++ java-native/src/test/java/org/aery/study/java/executors/NewSingleThreadExecutorStudy.java
@@ -1,6 +1,6 @@
 package org.aery.study.java.executors;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
