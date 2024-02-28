8f5a68fdf69538f3096ff2e49f8f6d9809f71613
Aery <nulllifeart@gmail.com>
2024-02-28 15:28:29 +0800

    add java-classloader submodule

---
+++ java-classloader/src/test/java/org/aery/study/java/classloader/ClassLoaderExposerTest.java
@@ -0,0 +1,12 @@
+package org.aery.study.java.classloader;
+
+import org.junit.Test;
+
+public class ClassLoaderExposerTest {
+
+    @Test
+    public void showParent() {
+        ClassLoaderExposer.showSystemClassLoaderParent();
+    }
+
+}
